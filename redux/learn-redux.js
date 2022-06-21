80a508827c67b5027dd14c2424a3e57773c0f576
Travis <travisbly@gmail.com>
2022-06-20 17:55:32 -0700

    added counter app

--- redux/learn-redux.js
+++ redux/learn-redux.js
@@ -1,10 +1,10 @@
 const obj = {
   a: {
     // To safely update obj.a.c, we have to copy each piece
-    c: 3
+    c: 3,
   },
-  b: 2
-}
+  b: 2,
+};
 
 const obj2 = {
   // copy obj
@@ -14,6 +14,19 @@ const obj2 = {
     // copy obj.a
     ...obj.a,
     // overwrite c
-    c: 42
-  }
-}
+    c: 42,
+  },
+};
+
+obj;
+obj2;
+
+const obj3 = {
+  ...obj,
+  b: 4,
+  a: {
+    c: 23,
+  },
+};
+
+obj3;
